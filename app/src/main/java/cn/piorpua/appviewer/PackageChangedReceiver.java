f0915cedd8f41a015c4e50b5c5d2f415d9749899
piorpua <yaxiong@mgtv.com>
2016-06-13 17:13:45 +0800

    +) PackageChangedReceiver

---
+++ app/src/main/java/cn/piorpua/appviewer/PackageChangedReceiver.java
@@ -0,0 +1,17 @@
+package cn.piorpua.appviewer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Author: piorpua<helloworld.hnu@gmail.com>
+ * Date: 16/6/13
+ */
+public final class PackageChangedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO
+    }
+}
